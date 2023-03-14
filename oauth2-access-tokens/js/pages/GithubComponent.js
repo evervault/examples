823f022f2b61e64b76e3a7120adfd4e973f7f7b1
Liz Moy <eclairemoy@gmail.com>
2023-03-14 10:18:38 +0000

    adding js

---
+++ oauth2-access-tokens/js/pages/GithubComponent.js
@@ -0,0 +1,20 @@
+import Image from 'next/image'
+
+export default function GitHubComponent() {
+    return (
+        <a
+            href={"https://github.com/login/oauth/authorize?scope=user:email&client_id=" + process.env.NEXT_PUBLIC_GITHUB_CLIENT_ID}
+            target="_blank"
+            rel="noopener noreferrer"
+        >
+        Sign up with {' '}
+        <Image
+            src="/github.png"
+            alt="Github Logo"
+            width={24}
+            height={24}
+            priority
+        />
+        </a>
+    );
+};
